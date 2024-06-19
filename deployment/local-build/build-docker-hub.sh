8238ff76ebd85644424e39c3a84b69e55e735f03
nesas-dev-01 <nesas-dev-01@localhost.localdomain>
2024-06-19 09:52:33 -0500

    Se agrega la pag de politicas de privacidad

--- deployment/local-build/build-docker-hub.sh
+++ deployment/local-build/build-docker-hub.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 source docker_hub_secrets.sh
 export DOCKERHUB_IMAGE=emi
-export DOCKERHUB_TAG=1.1.41
+export DOCKERHUB_TAG=1.1.46
 export DOCKERHUB_NAMESPACE=nebulaetpiprod
 export DOCKERHUB_IMAGE=emi
 
 echo "===== Building Docker Container ======"
-docker build  -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest .
+sudo docker build  -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest .
 
 echo "===== Publishing Docker Container ======"
-docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
-docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG
-docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest
+sudo docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
+sudo docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG
+sudo docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest
